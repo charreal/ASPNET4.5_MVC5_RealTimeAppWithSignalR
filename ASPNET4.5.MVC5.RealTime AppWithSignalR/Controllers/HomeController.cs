7f5ea49e14356b1a8710411f183ee3262e06e406
Cerberus27 <hairollucas@gmail.com>
2016-01-29 00:25:39 -0500

    Add Home Page
    Proyecto ASPNET 4.5 MVC5 con SignalR

---
+++ ASPNET4.5.MVC5.RealTime AppWithSignalR/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNET4._5.MVC5.RealTime_AppWithSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
